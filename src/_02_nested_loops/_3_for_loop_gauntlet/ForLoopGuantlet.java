165d784bf645fe5b106541f4c14ae28fae53dee1
student <student@ip-172-31-54-106.us-west-2.compute.internal>
2020-12-07 15:32:40 -0800

    finished for loop gauntlet

--- src/_02_nested_loops/_3_for_loop_gauntlet/ForLoopGuantlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/ForLoopGuantlet.java
@@ -39,7 +39,45 @@ public class ForLoopGuantlet {
 				System.out.println();
 		}
 		System.out.println();
-		
-	}
+		for(int h = 0; h<=2; h++) {
+			for(int h2 = 0; h2<=2; h2++) {
+				System.out.println(h+" "+h2);
 
+			}
+		}
+		System.out.println();
+		for(int i = 1; i<=9; i++) {
+			System.out.print(i);
+			if(i % 3 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+		int i4 = 1;
+		for(int i2 = 0; i2<=2; i2++) {
+			for(int i3 = 0; i3<=2; i3++) {
+				System.out.print(i4);
+				//					if(i4 % 3 == 0) {
+				//						System.out.println();
+				//					}
+				i4++;
+			}
+			System.out.println();
+		}
+		System.out.println();
+		int j = 1;
+		for(int j1 = 0; j1<=9; j1++) {
+			for(int j2 = 0; j2<=9; j2++) {
+				System.out.print(j+" ");
+				j++;
+			}
+			System.out.println();
+		}
+		for(int k = 1; k<=6; k++) {
+			for(int l = 1; l<=k; l++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
