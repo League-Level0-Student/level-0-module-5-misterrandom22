c3ee6c063b29d3fdfddd42fce514b8641378cd52
student <student@ip-172-31-60-22.us-west-2.compute.internal>
2021-01-11 15:31:33 -0800

    finished happy pet almost completed skill practice

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -10,24 +10,45 @@ public class HappyPet {
 	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-String pet = JOptionPane.showInputDialog("what kind of pet would you like to buy?");
+JOptionPane.showMessageDialog(null, "You now have a dog");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-
+while(happinessLevel <= 10) {
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
 			int task = JOptionPane.showOptionDialog(null, "What do you want to do with your pet?", "PetCare", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "cuddle", "food", "walk" }, null);
-System.out.println(task);
-			// 5. Use user input to call the appropriate method created in step 4.
 
+			// 5. Use user input to call the appropriate method created in step 4.
+if (task==0) {
+  cuddle();
+}
+if (task==1) {
+	  food();
+	}
+if (task==2) {
+	  walk();
+	}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-		
+
+}
+JOptionPane.showMessageDialog(null, "You and your dog are now best friends");
 	}
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
-	
+	public static void cuddle() {
+		JOptionPane.showMessageDialog(null, "the dog barks with joy");
+		happinessLevel = happinessLevel + 1;
+	}
+	public static void food() {
+		JOptionPane.showMessageDialog(null, "the dag feeds");
+		happinessLevel = happinessLevel + 2;
+	}
+	public static void walk() {
+		JOptionPane.showMessageDialog(null, "the dog prances");
+		happinessLevel = happinessLevel + 3;
+	}
 }
---
+++ src/_04_practice/_1_skill_practice/SkillPractice.java
@@ -0,0 +1,114 @@
+package _04_practice._1_skill_practice;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class SkillPractice {
+public static void main(String[] args) {
+
+SkillPractice skills = new SkillPractice();
+           //skills.skill1();
+           //skills.skill2();
+           skills.skill3();
+           skills.skill4();
+           skills.skill5();
+}
+
+void skill1() {
+// Use pop-ups for the following.
+// Ask the user how many dimes they have
+String dimes = JOptionPane.showInputDialog("how may dimes do you have?");
+
+
+
+// Tell them how many cents they have (hint multiply by 10)
+int cents = Integer.parseInt(dimes);
+cents=cents*10;
+JOptionPane.showMessageDialog(null, "You have "+cents+" cents");
+
+
+
+// Ask the user how tall they are (inches)
+String height = JOptionPane.showInputDialog("how tall are you in inches?");
+
+
+
+// If they are shorter than 36 inches, tell them "to eat their Wheaties" <- this was too british for me
+int inches = Integer.parseInt(height);
+if(inches <= 36) {
+JOptionPane.showMessageDialog(null, "drink milk or something, shortie");
+}
+
+
+
+}
+
+void skill2() { // Write a loop to print every third number between 1 and 30 to the console 
+
+for(int i=0; i<30; i++) {
+	if(i % 3== 0) {
+	System.out.print(i+" ");
+	}
+}
+
+
+
+
+
+}
+
+void skill3() { // Get a random number that is less than 20 and print it to the console 
+
+Random random1 = new Random();
+int num1 = random1.nextInt(20) +1;
+System.out.println(random1);
+
+// Get another random number that is less than 10 and print it to the console 
+Random random2 = new Random();
+int num2 = random2.nextInt(10) +1;
+System.out.println(random2);
+
+
+// Using a pop-up, tell the user the difference between the numbers // Hint: use subtraction 
+JOptionPane.showMessageDialog(null, num1-num2);
+
+
+}
+
+void skill4() { // In a pop-up, ask the user for the city they live in 
+
+
+
+// If they answered "San Diego", tell them they live in America's Finest City 
+
+
+
+// Otherwise, tell them to move to San Diego 
+
+
+
+// Create a variable - cars - and initialize it to the number of cars your family has. // If there are 0 cars, use a pop-up to display, "I bet you use public transportation." 
+
+
+
+// If there is 1 car, use a pop-up to display the make/model of the car 
+
+
+
+// If there is more than 1 car, use a pop-up to display how many wheels the // cars have between them. 
+
+
+
+}
+
+void skill5() { // In a pop-up, ask the user for the name of their school 
+
+
+
+// In another pop-up, tell the user, that their school is a fantastic school. // You must include the name of the school in the message. 
+
+
+
+}
+}
